e053a43e88b026cc3c752080905711f5a3c3864b
Simon DeMartini <simondemartini@gmail.com>
2017-01-16 17:04:22 -0800

    Add more changes talking about elephants

--- app/src/main/java/edu/uw/tacoma/tcss422/team4/lab_git/MainActivity.java
+++ app/src/main/java/edu/uw/tacoma/tcss422/team4/lab_git/MainActivity.java
@@ -3,9 +3,12 @@ package edu.uw.tacoma.tcss422.team4.lab_git;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,6 @@ public class MainActivity extends AppCompatActivity {
     public void showElephant(View view) {
         Intent intent = new Intent(this, DisplayElephantActivity.class);
         startActivity(intent);
+        Log.d(TAG, "A wild elephant appears");
     }
 }
